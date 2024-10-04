4f8c2aa8d1c5f4b5024dded5c49120a4e4467be0
rui623 <ruiiwata837@gmail.com>
2024-10-05 07:07:34 +0900

    update input code

--- manual_package/text_publisher.py
+++ manual_package/text_publisher.py
@@ -22,10 +22,23 @@ class TextPublisher(Node):
             img = np.zeros((720, 1280, 3))
             text=""   # パブリッシュするテキスト
             while True:
-                cv2.putText(img, text, (30, 30), cv2.FONT_HERSHEY_DUPLEX,
+                
+                # `upper_flg` に応じて異なる色の長方形を表示
+                if self.upper_flg:
+                    # 赤色の長方形 (左上: (50, 50), 右下: (250, 150))
+                    cv2.rectangle(img, (10, 10), (40, 50), (0, 0, 255), -1)
+                    cv2.putText(img, "T", (10, 35), cv2.FONT_HERSHEY_DUPLEX,
                             1.0, (255, 255, 255))
-                cv2.imshow("Text Publisher", img)
+                else:
+                    # 白色の長方形 (左上: (50, 50), 右下: (250, 150))
+                    cv2.rectangle(img, (10, 10), (40, 50), (255, 255, 255), -1)
+                    cv2.putText(img, "f", (10, 35), cv2.FONT_HERSHEY_DUPLEX,
+                            1.0, (0, 0, 0))
 
+                cv2.putText(img, text, (30, 75), cv2.FONT_HERSHEY_DUPLEX,
+                            1.0, (255, 255, 255))
+                
+                cv2.imshow("Text Publisher", img)
                 key = cv2.waitKey(10)
                 if key == 13:  # Enterキーが押された場合
                     print("pressed Enter Key")
@@ -34,6 +47,7 @@ class TextPublisher(Node):
                     break
                 if key == 225:
                     self.upper_flg = not self.upper_flg  # 大文字フラグを反転
+                    cv2.rectangle
                     continue
                 if key != -1:
                     if ord('a') <= key <= ord('z') and self.upper_flg:
